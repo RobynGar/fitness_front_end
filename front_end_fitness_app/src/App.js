3316d777a627fe106abd3a0a44d9b891404e3161
Wendy Hamenya <wendyhamenya@yahoo.co.uk>
2022-03-18 15:22:39 +0000

    created multiple tabs for webpage

--- front_end_fitness_app/src/App.js
+++ front_end_fitness_app/src/App.js
@@ -1,19 +1,23 @@
+import React from 'react';
 import './App.css';
 import Home from './Home';
-import RecipeBook from './RecipeBook';
-import MyRecipeBook from './MyRecipeBook';
+import RecipeBook from './RecipeBook';      //importing all additional webpages so they can be accessed from the main page
+import MyRecipeBook from './MyRecipeBook';  //required us to install react-router-dom in the terminal
 import Login from './Login';
 import SignUp from './SignUp';
+import {Route, Link, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <>
     <h1>Not My Fitness Pal</h1>
-    <Home />
-    <RecipeBook/>
-    <MyRecipeBook/>
-    <Login/>
-    <SignUp/>
+    <Routes>
+      <Route exact path= "/" element={<Home />} />
+      <Route exact path= "/RecipeBook" element={<RecipeBook />} />
+      <Route exact path= "/MyRecipeBook" element={<MyRecipeBook />} />
+      <Route exact path= "/Login" element={<Login />} />
+      <Route exact path= "/SignUp" element={<SignUp />} />
+    </Routes>
     </>
   );
 }
--- front_end_fitness_app/src/index.js
+++ front_end_fitness_app/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom'; //import the Browser Router which allows us to use the URL to navigate to different pages in our app
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function

d873a1647457713a94f934a12279133f90add3b0
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-03-21 15:30:08 +0000

    recipelist map

--- front_end_fitness_app/src/RecipeBook.js
+++ front_end_fitness_app/src/RecipeBook.js
@@ -1,13 +1,25 @@
 import Recipe from "./components/Recipe";
 
 const RecipeBook = ({recipeList}) => {
+    
     const recipeMap = recipeList.map(recipe => {
+            
         return <Recipe 
                     recipe={recipe} 
                     key={recipe.id} />
+            
+        
     }) 
-
-
+    function checkValue(input){
+        // recipeList.forEach(element => {
+        //     if(element.notes !== input){
+        //         return false;
+        //     }
+        // });
+        //let unique = array.filter((x, index) => array.indexOf(x) === index);
+// let duplicated = array.filter((x, index) => array.indexOf(x) !== index);
+    let unique = recipeList.filter((recipe, index) => recipeList.indexOf(recipe.notes) === index);
+    }
 
     return(
         <>
--- front_end_fitness_app/src/components/Recipe.js
+++ front_end_fitness_app/src/components/Recipe.js
@@ -8,6 +8,12 @@ const Recipe = ({recipe}) => {
         <> 
         {/* extracting name component from recipe prop */}
         <h2> {recipe.name} </h2>
+        <ul>
+            <li>{recipe.mealType}</li>
+            <li>{recipe.notes}</li>
+            <li>Calories: {recipe.calories}</li>
+            
+        </ul>
         </>
     )
 }

a95afee52da39b86489f7a41cfb169b5f255e4f7
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-03-21 11:58:52 +0000

    recipe refactor

--- front_end_fitness_app/src/App.js
+++ front_end_fitness_app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import Home from './Home';
 import RecipeBook from './RecipeBook';      //importing all additional webpages so they can be accessed from the main page
@@ -10,33 +10,16 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 
 function App() {
-  // food list as a state
-   const [foodList, setFoodList] = useState([]);
-
-   // fetch data from "localhost:8080/food/all"
-   useEffect(() => {
-     fetch("http://localhost:8080/food/all")
-      .then(response => response.json() )
-      .then(data => setFoodList(data))
-   },[])
-
-   // handleFoodFormSubmit 
-   const addRecipeToDatabase = (newRecipe) => {
-      fetch("http;//localhost:8080/food", {
-        method:"POST",
-        headers:{
-          "Content-Type": "application/json"
-        },
-        body:JSON.stringify(newRecipe)
-      })
-      .then(response => response.json())
-      .then(data => setFoodList([...foodList, data]))
-
-   }
+   const [foodList, setFoodList]  
 
   // log in ID as state in here 
 
 
+  // fetch data from "localhost:8080/food/all"
+
+  // handleFoodFormSubmit 
+
+  // food list as a state
   
 
 
@@ -46,8 +29,8 @@ function App() {
     <NavBar  />
     <Routes>
       <Route exact path= "/" element={<Home />} />
-      <Route exact path= "/RecipeBook" element={<RecipeBook foodList={foodList}/>} />
-      <Route exact path= "/MyRecipeBook" element={<MyRecipeBook foodList={foodList} onRecipeSubmission={addRecipeToDatabase}/>} />
+      <Route exact path= "/RecipeBook" element={<RecipeBook />} />
+      <Route exact path= "/MyRecipeBook" element={<MyRecipeBook />} />
       <Route exact path= "/Login" element={<Login />} />
       <Route exact path= "/SignUp" element={<SignUp />} />
     </Routes>

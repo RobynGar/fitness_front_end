0e9544e8b581bb76093f219993cfe1c3a7391792
Rosalinda Tome <rosalinda_tome@hotmail.com>
2022-03-21 13:11:49 +0000

    Recipe prop

--- front_end_fitness_app/src/components/Recipe.js
+++ front_end_fitness_app/src/components/Recipe.js
@@ -1,7 +1,18 @@
+
 // Don't print out personal ID, week or day
 
 // De-structure food object as a prop from RecipeBook 
-const Recipe = ({}) => {
+
+const Recipe = ({recipe}) => {
+    return(
+        <> 
+        {/* extracting name component from recipe prop */}
+        <h2> {recipe.name} </h2>
+        </>
+    )
+
 
 
 }
+
+export default Recipe;
